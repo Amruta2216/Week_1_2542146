12b2b345e1592111d052f229860d0554f5b2b707
Amruta2216 <amrutabandpatte13@gmail.com>
2026-01-04 14:52:58 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,11 @@
+def cir():
+	r=12
+	print("area of circle is: ",3.14*r*r)
+def tri():
+	b=3
+	h=7
+	print("area of triangle is: ",0.5*b*h)
+def squ(s):
+	print("area of square is: ",s*s)
+def rect(l,b):
+	print("area of rectangle is: ",l*b)
